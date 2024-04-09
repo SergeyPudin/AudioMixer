499c7f024b5b2ce0ddf638a1a5576336d280c59f
Pudin Sergey <Pudin.Sergey@mail.ru>
2024-04-09 20:53:45 +0300

    Sliders Logic

--- Assets/Scripts/OnOffButton.cs
+++ Assets/Scripts/OnOffButton.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
 public class OnOffButton : MonoBehaviour
 {
+    private const string Master = "Master";
+
+    [SerializeField] private AudioMixerGroup _mixer;
+    [SerializeField] private Color _onColor;
+    [SerializeField] private Color _offColor;
+
     private bool _isOn = true;  
     private Image _image;
 
@@ -17,17 +24,31 @@ public class OnOffButton : MonoBehaviour
     public void SwitchButton()
     {
         _isOn = !_isOn;
+
+        SwichMusic();
         UpdateButtonColor();
     }
 
     private void UpdateButtonColor()
     {
-        Color color = _isOn ? Color.white : Color.red;
+        Color color = _isOn ? _onColor : _offColor;
+
         ChangeButtonColor(color);
     }
 
     private void ChangeButtonColor(Color color)
     {
         _image.color = color;
+    }
+
+    private void SwichMusic()
+    {
+        float volume;
+        float maxValue = 0;
+        float minValue = -80;
+
+        volume = _isOn ? maxValue : minValue;
+
+        _mixer.audioMixer.SetFloat(Master, volume);
     }
 }
---
+++ Assets/Scripts/VolumeChanger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeChanger : MonoBehaviour
+{
+    [SerializeField] AudioMixerGroup _mixer;
+
+    public void ChangeVolume(float volume)
+    {
+        float maxValue = 0;
+        float minValue = -80;
+
+        _mixer.audioMixer.SetFloat(_mixer.name, Mathf.Lerp(minValue, maxValue, volume));
+    }
+}
