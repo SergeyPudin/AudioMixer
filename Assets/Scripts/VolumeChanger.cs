641eb74e214ade08c9f1101ca409507777b70a2d
SergeyPudin <Pudin.sergey@mail.ru>
2024-04-12 10:44:57 +0300

    Convert linear to decibel

--- Assets/Scripts/VolumeChanger.cs
+++ Assets/Scripts/VolumeChanger.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
+[RequireComponent (typeof(Slider))]
 public class VolumeChanger : MonoBehaviour
 {
     [SerializeField] AudioMixerGroup _mixer;
 
+    private Slider _slider;
+
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+
+        ChangeVolume(_slider.value);
+    }
+
     public void ChangeVolume(float volume)
     {
-        float maxValue = 0;
-        float minValue = -80;
+        _mixer.audioMixer.SetFloat(_mixer.name, ConvertToDecibel(volume));       
+    }
 
-        _mixer.audioMixer.SetFloat(_mixer.name, Mathf.Lerp(minValue, maxValue, volume));       
+    private float ConvertToDecibel(float volume)
+    {
+        float minValue = -80;
+        float multiplier = 20;
+
+        if (Mathf.Approximately(volume, 0))
+            return minValue; 
+
+        return Mathf.Log10(volume) * multiplier;
     }
 }
